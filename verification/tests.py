3dbf945df50ece935ee1898e569b748f90501527
XanderVi <engineermexanic@gmail.com>
2018-08-31 14:44:17 +0300

    add to GitHub

---
+++ verification/tests.py
@@ -0,0 +1,57 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input":[[[1, 6, 7, 2, 4],
+                      [0, 4, 9, 5, 3],
+                      [7, 2, 5, 1, 4],
+                      [3, 3, 2, 2, 9],
+                      [2, 6, 1, 4, 0]], 9],
+            "answer": 44
+        },
+        {
+            "input":[[[5, 6, 4, 3, 4],
+                      [2, 4, 9, 5, 3],
+                      [8, 2, 5, 7, 9],
+                      [3, 3, 2, 2, 6],
+                      [2, 6, 0, 4, 1]], 5],
+            "answer": 17
+        }
+    ],
+    "Extra": [
+        {
+            "input":[[[1, 2, 3, 4, 5],
+                      [2, 3, 4, 5, 1],
+                      [3, 4, 5, 1, 2],
+                      [4, 5, 1, 2, 3],
+                      [5, 1, 2, 3, 4]], 25],
+            "answer": 75
+        },
+        {
+            "input":[[[2, 5, 4, 1, 8],
+                      [0, 4, 9, 5, 3],
+                      [7, 2, 5, 1, 4],
+                      [3, 3, 2, 2, 9],
+                      [2, 6, 1, 4, 1]], 6],
+            "answer": 27
+        },
+        {
+            "input":[[[2, 5, 1, 2, 4],
+                      [0, 4, 9, 5, 3],
+                      [7, 4, 1, 1, 4],
+                      [3, 3, 2, 2, 9],
+                      [2, 6, 1, 4, 3]], 7],
+            "answer": 37
+        }
+
+    ]
+}
